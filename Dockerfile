a3f1d8a6f8abf9d9bf92252e46b785c7620609f5
Sprang, Richard <Richard.Sprang@toennies.de>
2026-02-23 16:00:00 +0100

    Revamped the base packages. Moved to Python Twisted Library

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,23 @@
-FROM ubuntu
+FROM python:3.12-slim
 
-# update
 RUN DEBIAN_FRONTEND=noninteractive \
-    apt-get update && \
-    apt-get install -y \
-    python3 \
-    python3-pip \
-    python3-venv \
-    openssh-client
-
-# create ssh keys
-RUN ssh-keygen -t rsa -b 2048 -f ~/.ssh/id_rsa -N ''
+    apt-get update && apt-get install -y openssh-client
 
 # app directory for our source files
 WORKDIR /app
 
+# create ssh keys
+RUN ssh-keygen -t rsa -b 2048 -f /app/id_rsa -N ''
+
 # install requirements
 COPY ./requirements.txt ./requirements.txt
-RUN python3 -m venv .venv
-RUN ./.venv/bin/pip install -r ./requirements.txt
+RUN pip3 install -r ./requirements.txt
 
 RUN mkdir log
 RUN touch log/Pinguin-Paradis.log
 
 # copy source files
-COPY src/ src/
-COPY main.py main.py
-
-CMD ["./.venv/bin/python3", "main.py"]
+COPY . .
 
 EXPOSE 22
+CMD ["python3", "main.py"]
--- main.py
+++ main.py
@@ -1,42 +1,133 @@
-import os, time
-from src.ssh_server import SshServer
-
-import logging
-
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s [%(levelname)s] [%(name)s] %(message)s",
-    handlers=[
-        logging.StreamHandler(),
-        logging.FileHandler("./log/Pinguin-Paradis.log")
-    ]
-)
-
-logger = logging.getLogger(__name__)
-
-
-if __name__ == '__main__':
-    #generation over cmd: "ssh-keygen -t rsa"
-    server = SshServer(os.path.expanduser('~/.ssh/id_rsa'))
-
-    # Start the server, you can give it a custom IP address and port, or
-    # leave it empty to run on 127.0.0.1:22
-    #server.start("0.0.0.0", int(os.getenv("SSH_PORT", 22)))
-    try:
-        #print("SSH Server startet – Strg+C zum Beenden")
-        logger.info("SSH Server startet – Strg+C zum Beenden")
+from twisted.conch import avatar, recvline
+from twisted.conch.interfaces import IConchUser, ISession
+from twisted.conch.ssh import factory, keys, session
+from twisted.conch.insults import insults
+from twisted.cred import portal, checkers
+from twisted.internet import reactor
+from zope.interface import implementer
+ 
+class SSHDemoProtocol(recvline.HistoricRecvLine):
+    def __init__(self, user):
+       self.user = user
+ 
+    def connectionMade(self):
+        recvline.HistoricRecvLine.connectionMade(self)
+        self.terminal.write("Welcome to my test SSH server.")
+        self.terminal.nextLine()
+        self.do_help()
+        self.showPrompt()
+ 
+    def showPrompt(self):
+        self.terminal.write("$ ")
+ 
+    def getCommandFunc(self, cmd):
+        return getattr(self, 'do_' + cmd.decode("utf-8"), None)
+ 
+    def lineReceived(self, line):
+        line = line.strip()
+        if line:
+            print(line)
+            f = open('logfile.log', 'w')
+            f.write(line.decode("utf-8"))
+            f.close
+            cmdAndArgs = line.split()
+            cmd = cmdAndArgs[0]
+            args = cmdAndArgs[1:]
+            func = self.getCommandFunc(cmd)
+            if func:
+                try:
+                    func(*args)
+                except Exception as e:
+                    self.terminal.write("Error: %s" % e)
+                    self.terminal.nextLine()
+            else:
+                self.terminal.write("No such command.")
+                self.terminal.nextLine()
+        self.showPrompt()
+ 
+    def do_help(self):
+        publicMethods = filter(
+            lambda funcname: funcname.startswith('do_'), dir(self))
+        commands = [cmd.replace('do_', '', 1) for cmd in publicMethods]
+        self.terminal.write("Commands: " + " ".join(commands))
+        self.terminal.nextLine()
+ 
+    def do_echo(self, *args):
+        self.terminal.write(" ".join(args))
+        self.terminal.nextLine()
+ 
+    def do_whoami(self):
+        self.terminal.write(self.user.username)
+        self.terminal.nextLine()
+ 
+    def do_quit(self):
+        self.terminal.write("Thanks for playing!")
+        self.terminal.nextLine()
+        self.terminal.loseConnection()
+ 
+    def do_clear(self):
+        self.terminal.reset()
         
-        server.start("0.0.0.0", int(os.getenv("SSH_PORT", 22)))
-        
-        # Falls start() nicht blockiert:
-        while True:
-            time.sleep(1)
+    def do_test(self):
+        self.terminal.write('Test')
+        self.terminal.nextLine()
+ 
+@implementer(ISession)
+class SSHDemoAvatar(avatar.ConchUser):
+ 
+ 
+    def __init__(self, username):
+        avatar.ConchUser.__init__(self)
+        self.username = username
+        self.channelLookup.update({b'session': session.SSHSession})
+ 
+ 
+    def openShell(self, protocol):
+        serverProtocol = insults.ServerProtocol(SSHDemoProtocol, self)
+        serverProtocol.makeConnection(protocol)
+        protocol.makeConnection(session.wrapProtocol(serverProtocol))
+ 
+ 
+    def getPty(self, terminal, windowSize, attrs):
+        return None
+ 
+ 
+    def execCommand(self, protocol, cmd):
+        raise NotImplementedError()
+ 
+    def closed(self):
+        pass
+ 
+@implementer(portal.IRealm)
+class SSHDemoRealm(object):
+     
+    def requestAvatar(self, avatarId, mind, *interfaces):
+        if IConchUser in interfaces:
+            return interfaces[0], SSHDemoAvatar(avatarId), lambda: None
+        else:
+            raise NotImplementedError("No supported interfaces found.")
+def getRSAKeys():
+    privateKey = keys.Key.fromFile("id_rsa")
+    publicKey = privateKey.public()
 
-    except KeyboardInterrupt:
-        logger.warning("KeyboardInterrupt empfangen – Server wird beendet...")
-        server.stop()
-        #print("\nKeyboardInterrupt empfangen – Server wird beendet...")
+    sshFactory.privateKeys = {
+        b"ssh-rsa": privateKey
+    }
 
-    finally:
-        server.stop()   # oder server.close(), je nach Implementierung
-        logger.info("Server gestoppt.")
+    sshFactory.publicKeys = {
+        b"ssh-rsa": publicKey
+    }
+    return sshFactory
+ 
+if __name__ == "__main__":
+    sshFactory = factory.SSHFactory()
+    sshFactory.portal = portal.Portal(SSHDemoRealm())
+ 
+
+sshFactory = getRSAKeys()
+users = {'admin': b'admin', 'guest': b'bbb'}
+sshFactory.portal.registerChecker(
+    checkers.InMemoryUsernamePasswordDatabaseDontUse(**users))
+
+reactor.listenTCP(22, sshFactory)
+reactor.run()
